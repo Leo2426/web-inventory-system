d2ce7ae1316d49867ce942622f9fde1b84f86409
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2024-06-01 14:23:24 -0500

    feat(sales): completed crud.

--- src/app/sales/components/table-sales/table-sales.component.ts
+++ src/app/sales/components/table-sales/table-sales.component.ts
@@ -3,44 +3,91 @@ import {ButtonModule} from "primeng/button";
 import {DialogModule} from "primeng/dialog";
 import {InputTextModule} from "primeng/inputtext";
 import {PaginatorModule} from "primeng/paginator";
-import {SharedModule} from "primeng/api";
+import {MessageService, SharedModule} from "primeng/api";
 import {TableModule} from "primeng/table";
 import {Sale} from "../../model/sale";
 import {SalesService} from "../../services/sales.service";
-
+import {ToastModule} from "primeng/toast";
 @Component({
   selector: 'app-table-sales',
   standalone: true,
-    imports: [
-        ButtonModule,
-        DialogModule,
-        InputTextModule,
-        PaginatorModule,
-        SharedModule,
-        TableModule
-    ],
+  imports: [
+    ButtonModule,
+    DialogModule,
+    InputTextModule,
+    PaginatorModule,
+    SharedModule,
+    TableModule,
+    ToastModule
+  ],
   templateUrl: './table-sales.component.html',
   styleUrl: './table-sales.component.css'
 })
-export class TableSalesComponent implements OnInit{
+export class TableSalesComponent implements OnInit {
   visibleEditForm: boolean = false;
   visibleAddForm: boolean = false;
   sales: Sale[] = [];
+  selectedSale: Sale = {} as Sale;
+  saleToEdit: Sale = {} as Sale;
+  saleToAdd: Sale = {} as Sale;
+  currentDate: string = '';
 
-  constructor(private saleService: SalesService) {
+  constructor(private saleService: SalesService, private messageService: MessageService) {
+    this.getCurrentDate();
+    this.saleToAdd = {
+      name: '',
+      saleDate: this.currentDate,
+      totalCost: 1
+    };
   }
+
   ngOnInit() {
     this.saleService.getAll().subscribe((response: any) => {
       this.sales = response;
     })
   }
 
-
-  onEditSale(sale: Sale) {
-
+  getCurrentDate() {
+    const date = new Date();
+    // fecha actual para generar la venta
+    this.currentDate = date.toLocaleDateString('es-ES', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    });
   }
 
-  deleteSaale(id: number) {
+  updateSale() {
+    this.saleService.update(this.saleToEdit.id, this.saleToEdit).subscribe((response: any) => {
+      this.sales = this.sales.map((sale) => {
+        if (sale.name === response.name) {
+          sale = response;
+        }
+        return sale;
+      });
+      this.visibleEditForm = false;
+      this.messageService.add({severity: 'success', summary: 'Success', detail: `Sale ${this.saleToEdit.name} updated successfully!` });
+    });
+  }
 
+  onEditSale(sale: Sale) {
+    this.saleToAdd = sale;
+    this.saleToEdit = {...sale};
+    this.visibleEditForm = true;
+  }
+
+  deleteSale(name: string) {
+    this.saleService.delete(name).subscribe(() => {
+      this.sales = this.sales.filter((sale) => sale.name !== name);
+    });
+  }
+
+  addSale() {
+    this.saleService.create(this.saleToAdd).subscribe((response: any) => {
+      this.sales.push(response);
+      this.messageService.add({severity: 'success', summary: 'Success', detail: `Sale ${this.saleToAdd.name} added successfully!` });
+    })
+
+    this.visibleAddForm = false;
   }
 }
