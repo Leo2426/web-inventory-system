54c86918579664f1a89a7229bee85fdab43aafae
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2024-06-06 11:27:07 -0500

    feat(sign-up): added sign up and fixed sign in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import {SidebarModule} from "primeng/sidebar";
 import {SidebarComponent} from "./shared/components/sidebar/sidebar.component";
 import {ToolbarComponent} from "./shared/components/toolbar/toolbar.component";
 import {SignInComponent} from "./public/pages/sign-in/sign-in.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarModule, SidebarComponent, ToolbarComponent, SignInComponent],
+  imports: [RouterOutlet, SidebarModule, SidebarComponent, ToolbarComponent, SignInComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/public/pages/sign-up/sign-up.component.ts
+++ src/app/public/pages/sign-up/sign-up.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from "primeng/button";
+import {CardModule} from "primeng/card";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterLink} from "@angular/router";
+import {ToastModule} from "primeng/toast";
+import {AuthService} from "../../../auth/services/auth.service";
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    RouterLink,
+    ToastModule,
+    FormsModule
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  userName: String;
+  lastName: String;
+  password: String;
+  email: String;
+
+  constructor(private authService: AuthService) {
+    this.userName = '';
+    this.lastName = '';
+    this.password = '';
+    this.email = '';
+  }
+
+  signUp() {
+    console.log('User Name: ' + this.userName);
+    console.log('Last Name: ' + this.lastName);
+    console.log('Email: ' + this.email);
+    console.log('Password: ' + this.password);
+
+    this.authService.create({
+      name: this.userName,
+      lastName: this.lastName,
+      email: this.email,
+      password: this.password
+    }).subscribe((response: any) => {
+      alert('User created successfully');
+      location.href = '/sign-in';
+    });
+
+  }
 
 }
