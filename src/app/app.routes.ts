c43a8608a300acc8a132a24a93598f948038bf35
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2024-06-06 11:02:51 -0500

    fix(sign-in): fixed protected routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,17 @@ import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-foun
 import {SalesComponent} from "./sales/pages/sales/sales.component";
 import {CustomersComponent} from "./customers/pages/customers/customers.component";
 import {SignInComponent} from "./public/pages/sign-in/sign-in.component";
+import {SignUpComponent} from "./public/pages/sign-up/sign-up.component";
+import {signInGuard} from "./auth/guards/signInGuard";
 
 
 export const routes: Routes = [
-  {path: 'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent, canActivate: [signInGuard]},
   {path: 'sign-in', component: SignInComponent},
   {path: '', redirectTo: 'sign-in', pathMatch: 'full'},
-  {path: 'inventory', component: InventoryComponent},
-  {path: 'sales', component: SalesComponent},
-  {path: 'customers', component: CustomersComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'inventory', component: InventoryComponent, canActivate: [signInGuard]},
+  {path: 'sales', component: SalesComponent, canActivate: [signInGuard]},
+  {path: 'customers', component: CustomersComponent, canActivate: [signInGuard]},
   {path: '**', component: PageNotFoundComponent}
 ];
