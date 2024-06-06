235943477126a6f94cdb0a685b62a6c54b179702
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2024-06-06 09:44:10 -0500

    feat(home): added total sales to home.

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SidebarComponent} from "../../../shared/components/sidebar/sidebar.component";
 import {ButtonModule} from "primeng/button";
+import {SalesService} from "../../../sales/services/sales.service";
+import {Sale} from "../../../sales/model/sale";
+import {CardModule} from "primeng/card";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     SidebarComponent,
-    ButtonModule
+    ButtonModule,
+    CardModule,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  totalSales: number = 0;
+  constructor(private saleService: SalesService) {
+  }
 
+  ngOnInit() {
+    this.saleService.getAll().subscribe((response: any) => {
+      this.totalSales = response.reduce((acc: number, sale: any) => {
+        return acc + sale.totalCost;
+      }, 0);
+    })
+  }
 
 }
