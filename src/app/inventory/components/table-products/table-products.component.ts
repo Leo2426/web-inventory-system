4519b0a2c74ca51519906a049c01e572c6ae3850
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2024-06-13 00:05:18 -0500

    fix(inventory): fixed products source.

--- src/app/inventory/components/table-products/table-products.component.ts
+++ src/app/inventory/components/table-products/table-products.component.ts
@@ -1,4 +1,4 @@
-import {Component, contentChild, input, OnInit, viewChild} from '@angular/core';
+import {Component, contentChild, input, OnDestroy, OnInit, viewChild} from '@angular/core';
 import {TableModule} from "primeng/table";
 import {ButtonModule} from "primeng/button";
 import {ToastModule} from "primeng/toast";
@@ -8,7 +8,6 @@ import {DialogModule} from "primeng/dialog";
 import {ChipsModule} from "primeng/chips";
 import {Product} from "../../model/product";
 import {FormsModule} from "@angular/forms";
-import {ProductsService} from "../../services/products.service";
 @Component({
   selector: 'app-table-products',
   standalone: true,
@@ -24,69 +23,111 @@ import {ProductsService} from "../../services/products.service";
   templateUrl: './table-products.component.html',
   styleUrl: './table-products.component.css'
 })
-export class TableProductsComponent implements OnInit{
+export class TableProductsComponent implements OnInit, OnDestroy{
   products: Product[] = [];
+
   visibleEditForm: boolean = false;
   visibleAddForm: boolean = false;
   selectedProduct: Product = {} as Product;
-  productToAdd: Product;
+  productToAdd: Product = {} as Product;
   productToEdit: Product = {} as Product;
   ngOnInit() {
     this.getAllProducts();
   }
 
 
-  constructor(private productService: ProductsService ,  private messageService: MessageService) {
-    this.productToAdd = {
-      name: '',
-      unitPrice: 0,
-      realPrice: 0,
-      stock: 0,
-      currentStock: 0,
-      userId: 1,
-    };
+  constructor( private messageService: MessageService) {
+   //si products en el local storage es null, entonces setear products
+    const initialProducts = [
+      {
+        id: 1,
+        name: 'product 1',
+        unitPrice: 100,
+        realPrice: 120,
+        discount: 0,
+        stock: 100,
+        userId: 1,
+      },
+      {
+        id: 2,
+        name: 'product 2',
+        unitPrice: 200,
+        realPrice: 240,
+        discount: 0,
+        stock: 200,
+        userId: 1,
+      },
+      {
+        id: 3,
+        name: 'product 3',
+        unitPrice: 300,
+        realPrice: 350,
+        discount: 0,
+        stock: 300,
+        userId: 1,
+      },
+
+    ]
+
+    if (localStorage.getItem('products') === null) {
+      localStorage.setItem('products', JSON.stringify(initialProducts));
+    }
+
+
+    this.resetProductToAdd()
   }
 
   deleteProduct(id: number) {
-    this.productService.delete(id).subscribe(() => {
-      this.products = this.products.filter((product) => product.id !== id);
-    });
+    this.products = this.products.filter((product: Product) => product.id !== id);
   }
 
   updateProduct() {
-    this.productService.update(this.productToEdit.id, this.productToEdit).subscribe((response: any) => {
-       this.products = this.products.map((product) => {
-        if (product.id === response.id) {
-          product = response;
-        }
-        return product;
-
-      });
-        console.log(response);
+    this.products = this.products.map((product: Product) => {
+      if (product.id === this.productToEdit.id) {
+        return this.productToEdit;
+      }
+      return product;
     });
 
     this.visibleEditForm = false;
   }
 
   addProduct() {
-    this.productService.create(this.productToAdd).subscribe((response: any) => {
-      console.log(response);
-      this.products.push(response);
-    });
+    //aumentar el id en 1
+    this.productToAdd.id = this.products[this.products.length - 1].id + 1;
+    this.products.push({...this.productToAdd});
+
+    //resetear el productToAdd
+    this.resetProductToAdd()
+
     this.visibleAddForm = false;
   }
 
 
   private getAllProducts() {
-    this.productService.getAll().subscribe((response: any) => {
-      this.products = response;
-    });
+    this.products = JSON.parse(localStorage.getItem('products') || '{}');
   }
 
   onEditProduct(product: Product) {
     this.selectedProduct = product;
     this.productToEdit = {...product};
     this.visibleEditForm = true;
+  }
+
+  resetProductToAdd() {
+    this.productToAdd = {
+      id: 0,
+      name: '',
+      unitPrice: 0,
+      realPrice: 0,
+      discount: 0,
+      stock: 0,
+      userId: 1,
+    };
+  }
+
+  ngOnDestroy() {
+    localStorage.setItem('products', JSON.stringify(this.products));
   }
 
 }
--- src/app/inventory/model/product.ts
+++ src/app/inventory/model/product.ts
@@ -1,21 +1,19 @@
 export class Product {
-  id?: number
+  id: number
   name: string
   unitPrice: number
   realPrice: number
   discount?: number
   stock: number
-  currentStock:  number
   userId: number
 
-  constructor( discount:number, id: number, name: string, unitPrice: number, realPrice: number, stock: number, currentStock: number, userId: number) {
+  constructor( discount:number, id: number, name: string, unitPrice: number, realPrice: number, stock: number, userId: number) {
     this.id = id
     this.discount = discount
     this.name = name
     this.unitPrice = unitPrice
     this.realPrice = realPrice
     this.stock = stock
-    this.currentStock = currentStock
     this.userId = userId
   }
 }
